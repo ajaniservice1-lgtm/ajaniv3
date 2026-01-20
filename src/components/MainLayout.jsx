dab3a2e8e5316611d3b72fbc65f173adc973aa92
Ajani <ajaniservive1@gmail.com>
2026-01-20 11:31:23 +0100

    fix

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -4,8 +4,8 @@ import Header from './Header';
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-white">
-      <Header />
+    <div className="min-h-screen ">
+      {/* <Header /> */}
       {/* This empty div pushes content down below the fixed header */}
       <div className="h-16"></div>
       <main className="min-h-[calc(100vh-4rem)]">
